e29f909a791640eb3fcaaa18fc90afc948936a14
IMRAN AHMED <project.developer.tech@gmail.com>
2026-08-05 14:21:39 +0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ import { el } from './core/dom.js';
 import { buildHeader } from './sections/header.js';
 import { buildHeroBanner } from './sections/heroBanner.js';
 import { buildHero } from './sections/hero.js';
+import { buildShowcase } from './sections/showcase.js';
 import { buildAbout } from './sections/about.js';
 import { buildProjectsSection } from './sections/projects.js';
 import { buildContactSection } from './sections/contact.js';
@@ -21,6 +22,7 @@ export function renderApp(root) {
     buildHeader(),
     buildHeroBanner(),
     buildHero(),
+    buildShowcase(),
     buildAbout(),
     buildProjectsSection(),
     buildContactSection(),
